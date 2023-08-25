8952965754ec71f5d3d88035e96b33428368e762
Mohd. Salahudeen <hudeensala948@gmail.com>
2023-08-25 12:24:46 +0530

    init

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles/header.css";
+
+function Header() {
+  return <div className="header"></div>;
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./styles/main.css";
+
+function Main() {
+  return (
+    <div className="main">
+      <div className="main__left">
+        <h2 className="text1">
+          Lab n <span>Box</span>
+        </h2>
+        <h2 className="text2">
+          Unleash Your
+          <i>
+            Inner Explorers <span><strong>!</strong></span>
+          </i>
+        </h2>
+        <h4>
+          Discover the fusion of Steamtastic education and <br />
+          practical experience within the virtual world.
+        </h4>
+        <a href="#">Join the Lab n Box Community &#8594;</a>
+      </div>
+      <div className="main__right">
+        <img className="img1" src="./img/vr.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Main;
